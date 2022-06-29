d7e2b4f09f1875cd7137fbc193e1851a5725690c
Mohammed-AlSharafi <83011045+Mohammed-AlSharafi@users.noreply.github.com>
2022-06-29 18:26:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,22 +15,25 @@ namespace RestaurantEx
                 new ManagerModel("John", "Smith"),
                 new WaiterModel("James", "Long"),
                 new ChefModel("Don", "Chris")
-        };
+            };
             List<IEmployee> emperorEmployees = new List<IEmployee>()
             {
                 new ManagerModel("Jana", "Brown"),
                 new WaiterModel("Soy", "Lang"),
                 new ChefModel("Samantha", "Adams")
-        };
+            };
+
             List<IRestaurant> restaurants = new List<IRestaurant>
             {
                 new Restaurant("Sana'a Restaurant", 10, sanaaEmployees),
                 new Restaurant("Empror's Restaurant", 8, emperorEmployees)
-        };
+            };
+
             foreach(var restaurant in restaurants)
             {
                 restaurant.GetInfo();
             }
+
             Console.ReadLine();
         }
     }
